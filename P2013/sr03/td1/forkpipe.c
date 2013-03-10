a0c27e1df64256693f1b8329b958301fc7da6857
mick <mickmacf16@hotmail.com>
2013-03-10 17:12:54 +0100

    un peu de reorganisation

--- P2013/sr03/td1/forkpipe.c
+++ P2013/sr03/td1/forkpipe.c
@@ -8,7 +8,64 @@
 
 int main (void)
 {
-	char machaine[100];
-	strcpy(machaine,"qsdsqdqsd");
-	printf(machaine);
+
+  pid_t pid1,pid2;
+  char machaine[100];
+  
+  int taille,ret;
+  FILE *fichier = NULL;  
+  int p[2];
+  if (pipe(p) == -1)
+  { 
+      perror ("creation pipe impossible\n");
+      exit(0);
+  }
+  else
+  {
+
+    pid1 = fork();
+    switch(pid1)
+    {
+      case -1:	perror("Creation impossible");
+		break;
+      
+      case 0:	//code du fils1
+		printf("fils 1");
+		close(p[0]);
+		fichier = fopen("input.txt","r");
+		do
+		{
+		    ret = fgets(machaine,100,fichier);
+		    taille = strlen(machaine);
+		    sprintf(machaine,"[%03d][%s]",taille);
+		    write(p[1], machaine, strlen(machaine));
+		}
+		while(ret != EOF);
+		fclose();
+		close(p[1]);
+      default:
+		pid2 = fork();
+		switch(pid2)
+		{
+		    case -1: 	perror("Creation impossible");
+				break;
+		    
+		    case 0:	//code du fil2
+				printf("fils 2");
+				close(p[1]);	
+
+
+				close(p[0]);
+				break;
+		    default:
+				printf("pere");
+				close(p[0]);
+				close(p[1]);
+				waitpid(pid1);
+				waitpid(pid2);
+				exit(0)
+		  
+				
+		}
+    }
 }
