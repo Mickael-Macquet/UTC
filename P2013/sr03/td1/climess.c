0d56b4dd42aab223643e39ad1e1fa2a988efddac
mick <mickmacf16@hotmail.com>
2013-03-13 01:00:48 +0100

    sr03 ex 2 ok

--- P2013/sr03/td1/climess.c
+++ P2013/sr03/td1/climess.c
@@ -5,32 +5,115 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
+typedef struct{
+    long type; //2 = serveur, 3 = client
+    int question;
+    int nucli;
+    char product_list[100];
+} message;
 
-int main(void):
+int main(void)
 {
-  key_t clef;
-  int id_msg;
-  longmsg = sizeof(message) - sizeof(long);
-  
-  clef= ftok("sr03p025", 0)
-  id_msg = msgget(clef, 0);
-  if(id_msg == -1)
-    perror("recuperation file impossible");
-    exit();
-  //demande d'identifiant
-  m.type= 2;
-  m.question = -1;
-  msgsnd(id_msg,(void*)&msg,longmsg,0);
-  msgrcv(id_msg,(void*)&msg,longmsg,3,0)
-  num_cli= msg.numcli
-  
-  //demande de liste de produit
-  msg.type=2;
-  msg.question=1
-  msgsnd(id_msg,(void*)&msg,longmsg,0);
-  msgrcv(id_msg,(void*)&msg,longmsg,numcli,0)
-  
-  
-  
+	key_t clef;
+	int id_msg,choix;
+	int num_cli = 0;
+	int longmsg = sizeof(message) - sizeof(long);
+	message msg;
+	clef= ftok("./sr03p025", 0);
+
+	//On recupere l'identifiant de la file du message
+	id_msg = msgget(clef,0);
+
+	if(id_msg == -1){
+		perror("recuperation file impossible\n");
+		exit(1);
+	}
+
+    while(choix!=0)
+    {
+        printf("1 : Demander un identifiant.\n");
+        printf("2 : Demander la liste des produits.\n");
+        printf("0 : Quitter.\n");
+        printf("Quel est votre choix ? : \n");
+        scanf("%d",&choix);
+		switch(choix)
+		{
+			//demande d'identifiant
+			case 1: 
+			{
+				if(num_cli)
+					printf("Je suis le client identifié: %d. Pas besoin de le refaire :p\n", msg.nucli);
+				else
+				{
+					// Type 2 car on demande au serveur.
+					msg.type= 2;
+					
+					//Question -1 ( voir serveur demande d'id )
+					msg.question = -1;
+					
+					//On envoi le message.
+					msgsnd(id_msg,(void*)&msg,longmsg,0);
+					
+					//On attend la reception de message de type 3 (pour client non identifié)
+					msgrcv(id_msg,(void*)&msg,longmsg,3,0);
+					
+					if (msg.nucli == -1)
+					{
+						printf("Le serveur ne prend plus de client Bye: %d\n", msg.nucli);
+						exit(0);
+					}
+					else
+						if(msg.nucli == -2)
+							printf("Le serveur a trop de client reesai plus tard: %d\n", msg.nucli);
+						else
+						{
+							num_cli = msg.nucli;
+							printf("Je suis le client identifié: %d\n", msg.nucli);
+						}
+				}
+				
+			}break;
+			//demande de liste de produit
+			case 2: 
+			{
+				if(num_cli)
+				{
+					// Type 2 car on demande au serveur.
+					msg.type=2;
+					
+					//Question 2 ( voir serveur, demande de la liste )
+					msg.question=0;
+					
+					//On envoi le message.
+					msgsnd(id_msg,(void*)&msg,longmsg,0);
+					
+					//On attend la reception de message du type num_cli ( seul le client qui a fait la demande pourra donc le lire ).
+					msgrcv(id_msg,(void*)&msg,longmsg,num_cli,0);
+					printf("Je suis le client: %d\n Voici la liste des produits %s\n", num_cli,msg.product_list);
+				}
+				else
+					printf("Il faut que je m'identifie d'abors\n");
+			}break;
+			case 0:
+			{
+				// Type 2 car on demande au serveur.
+				msg.type=2;
+				
+				//Question 2 ( voir serveur, demande de la liste )
+				msg.question=1;
+				
+				//On envoi le message.
+				msgsnd(id_msg,(void*)&msg,longmsg,0);
+				
+				//On attend la reception de message du type num_cli ( seul le client qui a fait la demande pourra donc le lire ).
+				msgrcv(id_msg,(void*)&msg,longmsg,num_cli,0);
+				
+				printf("J'ai dis au serveur Bye :p\n");
+			}break;
+		}
+	}
 }
--- P2013/sr03/td1/servmess.c
+++ P2013/sr03/td1/servmess.c
@@ -5,41 +5,100 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-#define REQ_NUM_CLI = -1 blabla
-
-typedef struct
-{
-    long type;
+typedef struct{
+    long type; //2 = serveur, 3 = client
     int question;
-    int numclt;
-    
-}message
+    int nucli;
+    char product_list[100];
+} message;
 
-
-int main(void):
+int main(void)
 {
-  key_t clef;
-  int id_msg;
-  message msg;
-  longmsg = sizeof(message) - sizeof(long);
+	key_t clef= ftok("./sr03p025", 0);
+	int id_msg;
+	int nombre_cli = 0;
+	message msg;
+	int longmsg = sizeof(message) - sizeof(long);
+	int last_id = 1;
+	int nombre_cli_simult = 0;
   
-  clef= ftok("sr03p025", 0)
-  id_msg = msgget(clef, IPC_CREAT|IPC_EXCLT|0666);
-  if(id_msg == -1)
-    perror("Creation file impossible");
-    exit();
-    //msgsnd
-    //msgrcv
-    msgrcv(id_msg,(void*)&msg,long-msg, 2,p)
-    switch(message.question)
-      case -1:
-	  //affectation id client
-	  msg.type=3;
-	  msg.numclt = //le numero que l'on veut filer au client
-	  msgsnd(id_msg,(void*)&msg, longmsg, 0);
-      case 0:
-	  //list produit
-	  msg.type = msg.numclt
-	  
+	//On crée notre file de message ( on crée et on s'assure qu'aucun n'est crée )
+	id_msg = msgget(clef, IPC_CREAT|IPC_EXCL|0666);
+	
+	if(id_msg == -1)
+	{
+		perror("Creation file impossible\n");
+		exit(0);
+	}
+	
+	//On traite un nombre limité de demande ( 4 ).
+    while(nombre_cli < 4 | nombre_cli_simult > 0)
+    {
+		printf("nombre client  : %d\n",nombre_cli);
+		//On attend la reception de messages venant d'un client(le type est set a 2 quand on envoi vers le serveur)
+		msgrcv(id_msg,(void*)&msg,longmsg, 2,0);
+		
+		switch(msg.question)
+		{
+			//affectation id client
+			case -1:
+			{	
+				if (nombre_cli < 4 & nombre_cli_simult < 2 )
+				{
+					//on incremente le nombre de client en train de demander des choses.
+					nombre_cli++;
+					
+					//on incremente le nombre de client identifié en meme temps.
+					nombre_cli_simult++;
+					msg.type = 3;
+					msg.nucli = last_id++;//le numero que l'on veut filer au client
+					msgsnd(id_msg,(void*)&msg, longmsg, 0);
+					printf("Je vien d'identifier le client: %d\n", msg.nucli);
+				}
+				else
+				{
+					msg.type = 3;
+					if(nombre_cli >= 4)
+						//On ferme le client ( plus possible de s'identifier )
+						msg.nucli = -1;
+					else
+						//On le met en attente ( ya juste 2 client simult )
+						msg.nucli = -2;
+					msgsnd(id_msg,(void*)&msg, longmsg, 0);
+					printf("Je vien d'envoyer balader un client\n", msg.nucli);	
+				}
+			}break;
+			//demande de la liste
+			case 0:
+			{
+					//On utilise le numero du client actuel comme type.
+					msg.type = msg.nucli;
+					printf("numero du client : %d\n",msg.type);
+					
+					//On envoi une chaine faisant etat des stocks;
+					strcpy(msg.product_list, "1,poireaux,25,0,5\n2,carottes,36,0,8\n3,navets,15,0,3\n");
+					
+					msgsnd(id_msg,(void*)&msg, longmsg, 0);
+					printf("coucou j'ai envoyé (serv) %s\n", msg.product_list);
+
+			}break;
+			
+			//Un client s'en va.
+			case 1:
+			{
+				//On utilise le numero du client actuel comme type.
+				msg.type = msg.nucli;
+				printf("le client : %d se casse\n",msg.type);
+				msgsnd(id_msg,(void*)&msg, longmsg, 0);
+				nombre_cli_simult--;
+				
+			}break;
+			
+		}
+	}
+	msgctl(id_msg,IPC_RMID,NULL);
 }
